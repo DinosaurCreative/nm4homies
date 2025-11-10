c46a258f0af9f879e3aa47878b148fde32e459b6
ggimaev <ggimaev@emphasoft.com>
2025-11-10 18:08:56 +0300

    feat: added new database

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,31 +1,33 @@
-import axios from 'axios';
-const mainUrl = process.env.REACT_APP_MAIN_URL;
+import axios from "axios";
+const mainUrl = process.env.REACT_APP_MAIN_URL || "http://localhost:3000";
 
+const myFetch = async (path, options) => {
+    const response = await axios.request({
+        baseURL: mainUrl + path,
+        data: options?.body,
+        method: options?.method,
+        withCredentials: true,
+        credentials: "include"
+    });
 
-const myFetch = async (path, options) =>{
-  const response = await axios.request({
-    baseURL: mainUrl + path,
-    data: options?.body,
-    method: options?.method,
-    withCredentials: true,
-    credentials: 'include',
-  });
-
-  return response;
+    return response;
 };
-export const checkToken = () => myFetch('/user');
+export const checkToken = () => myFetch("/user");
 
-export const deleteStickerPack = id => myFetch(`/sticker-pack/${id}`, { method: 'DELETE'});
+export const deleteStickerPack = (id) =>
+    myFetch(`/sticker-pack/${id}`, { method: "DELETE" });
 
-export const changeStickerPack = body => myFetch('/sticker-pack', { method: 'PATCH' });
+export const changeStickerPack = (body) =>
+    myFetch("/sticker-pack", { method: "PATCH" });
 
-export const getAllStickerPacks = body => myFetch('/sticker-packs');
+export const getAllStickerPacks = (body) => myFetch("/sticker-packs");
 
-export const login = password => myFetch('/signin', { method: 'POST', body: {password} });
+export const login = (password) =>
+    myFetch("/signin", { method: "POST", body: { password } });
 
-export const signout = () => myFetch('/signout', { method: 'DELETE' });
+export const signout = () => myFetch("/signout", { method: "DELETE" });
 
-export const getStickerPack = body => myFetch('/sticker-pack');
+export const getStickerPack = (body) => myFetch("/sticker-pack");
 
-export const createStickerPack = body => myFetch('/sticker-pack', { method: 'POST', body });
-
+export const createStickerPack = (body) =>
+    myFetch("/sticker-pack", { method: "POST", body });
